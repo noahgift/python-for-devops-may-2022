7ec05f2e7548c3f814e5c147c3761986a6977776
Noah Gift <noah.gift@gmail.com>
2022-05-05 13:37:19 +0000

    adding successful test

--- Makefile
+++ Makefile
@@ -3,3 +3,5 @@ install:
 		pip install -r requirements.txt
 lint:
 	pylint --disable=R,C *.py devopslib
+test:
+	python -m pytest -vv --cov=devopslib test_*.py
---
+++ test_devopslib.py
@@ -0,0 +1,5 @@
+from devopslib.randomfruit import fruit
+
+def test_fruit():
+    fruit_choice = fruit()
+    assert fruit_choice in ["apple", "cherry", "strawberry"]
